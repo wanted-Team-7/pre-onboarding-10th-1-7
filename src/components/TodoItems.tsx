7adcb7104adedd0d5364a73e144a4ba73d488710
daedaem <tg8685@gmail.com>
2023-04-08 19:24:58 +0900

    Add Todo 추가, 삭제, 수정, 체크수정 완료

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -1,9 +1,65 @@
-const TodoItems = () => {
+import { useState, useRef } from "react";
+interface TodoItem {
+  id: number;
+  todo: string;
+  isCompleted: boolean;
+  userId: number;
+}
+
+interface TodoItemsProps {
+  todoItem: TodoItem;
+  onDelete: (id: number) => void;
+  onUpdate: (id: number, todoText: string, isCompleted: boolean) => void;
+}
+const TodoItems: React.FC<TodoItemsProps> = ({
+  todoItem,
+  onDelete,
+  onUpdate,
+}) => {
+  const [isUpdate, setIsUpdate] = useState(false);
+  const newTodoRef = useRef<HTMLInputElement>(null);
+  const deleteTodoHandler = () => {
+    onDelete(todoItem.id);
+  };
+  const updateTodoHandler = () => {
+    setIsUpdate(() => true);
+    const newTodo = newTodoRef.current?.value;
+    if (!newTodo || newTodo.trim().length === 0) return;
+    onUpdate(todoItem.id, newTodo, todoItem.isCompleted);
+    setIsUpdate(() => false);
+  };
+  const todoCompleteHandler = () => {
+    onUpdate(todoItem.id, todoItem.todo, !todoItem.isCompleted);
+  };
   return (
     <li>
       <label>
-        <input type="checkbox" />
-        <span>TODO 1</span>
+        <input
+          type="checkbox"
+          defaultChecked={todoItem.isCompleted}
+          onClick={todoCompleteHandler}
+        />
+        {!isUpdate && <span>{todoItem.todo}</span>}
+        {isUpdate && (
+          <input
+            data-testid="modify-input"
+            ref={newTodoRef}
+            onClick={updateTodoHandler}
+          />
+        )}
+        {isUpdate ? (
+          <button data-testid="submit-button" onClick={updateTodoHandler}>
+            제출
+          </button>
+        ) : (
+          <button data-testid="modify-button" onClick={updateTodoHandler}>
+            수정
+          </button>
+        )}
+
+        <button data-testid="delete-button" onClick={deleteTodoHandler}>
+          삭제
+        </button>
       </label>
     </li>
   );
--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,14 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import TodoItems from "../components/TodoItems";
 
-interface fetchDataType {
-  email: string;
-  password: string;
+interface TodoItem {
+  id: number;
+  todo: string;
+  isCompleted: boolean;
+  userId: number;
 }
+
 const Todos: React.FC = () => {
-  const [todoItems, setTodoItems] = useState([]);
-  useEffect(() => {
-    // const { email, password } = JSON.parse(localStorage.getItem("'access_token'"));
-    const getTodos = async () => {
+  const [todoItems, setTodoItems] = useState<TodoItem[]>([]);
+  const newTodoRef = useRef<HTMLInputElement>(null);
+  const getTodos = async () => {
+    try {
       const response = await fetch(
         "https://www.pre-onboarding-selection-task.shop/todos",
         {
@@ -25,13 +29,114 @@ const Todos: React.FC = () => {
         const fetchedData = await response.json();
         setTodoItems(() => fetchedData);
       }
-    };
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  };
+  useEffect(() => {
     getTodos();
   }, []);
+  const createTodo = async () => {
+    try {
+      const response = await fetch(
+        "https://www.pre-onboarding-selection-task.shop/todos",
+        {
+          method: "post",
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("access_token")}`,
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({ todo: newTodoRef.current!.value }),
+        }
+      );
+      if (!response.ok) {
+        const errorResponse = await response.json();
+        throw new Error(errorResponse.message);
+      }
+    } catch (error: any) {
+      console.error(error.message);
+    }
+    newTodoRef.current!.value = "";
+
+    getTodos();
+  };
+  const updateTodo = async (
+    todoId: number,
+    todoText: string,
+    isCompleted: boolean
+  ) => {
+    try {
+      const response = await fetch(
+        `https://www.pre-onboarding-selection-task.shop/todos/${todoId}`,
+        {
+          method: "put",
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("access_token")}`,
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({
+            todo: todoText,
+            isCompleted: isCompleted,
+          }),
+        }
+      );
+      if (!response.ok) {
+        const errorResponse = await response.json();
+        throw new Error(errorResponse.message);
+      }
+    } catch (error: any) {
+      console.error(error.message);
+    }
+    getTodos();
+  };
+
+  const deleteTodo = async (todoId: number) => {
+    try {
+      const response = await fetch(
+        `https://www.pre-onboarding-selection-task.shop/todos/${todoId}`,
+        {
+          method: "delete",
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
+        }
+      );
+      if (!response.ok) {
+        const errorResponse = await response.json();
+        throw new Error(errorResponse.message);
+      }
+    } catch (error: any) {
+      console.error(error.message);
+    }
+    getTodos();
+  };
+  const todoSubmitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    const newTodo = newTodoRef.current!.value;
+    if (newTodo.trim() === "") return;
+    createTodo();
+  };
+
   console.log(todoItems);
   return (
     <section>
-      <article></article>
+      <form onSubmit={todoSubmitHandler}>
+        <input data-testid="new-todo-input" type="text" ref={newTodoRef} />
+        <button data-testid="new-todo-add-button" type="submit">
+          추가
+        </button>
+      </form>
+      <article>
+        {todoItems &&
+          todoItems.map((el) => (
+            <TodoItems
+              key={el.id}
+              todoItem={el}
+              onDelete={deleteTodo}
+              onUpdate={updateTodo}
+            />
+          ))}
+      </article>
     </section>
   );
 };
