677f67859c89dba4e09b873795a087e482203527
daedaem <tg8685@gmail.com>
2023-04-11 20:35:04 +0900

    Deploy master branch

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -2,13 +2,13 @@ import { useRouteError } from "react-router-dom";
 import PageContent from "../components/PageContent";
 
 const ErrorPage = () => {
-  const error = useRouteError();
+  const error: any = useRouteError();
   let title = "에러가 발생했습니다!";
   let message = "문제가 발생하였습니다.";
   if (error.status === 500) {
     message = error.data.message;
   }
-  if (error?.status === 404) {
+  if (error.status === 404) {
     title = "404 에러";
     message = "요청 하신 페이지는 찾을 수 없습니다.";
   }
