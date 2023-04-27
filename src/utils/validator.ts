5bd5840f0f3b3d21668e525b1baad1694c14c34c
Withlaw <car2559@naver.com>
2023-04-27 14:01:01 +0900

    refactor: 유효성검사함수에서 에러메시지 반환하도록 수정

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,9 +1,16 @@
-export const validateEmail = (email: string) => {
-  return email.trim().includes('@');
+const ERROR_MESSAGE = {
+  EMAIL: '공백없이 @를 포함한 올바른 이메일 주소를 입력해주세요.',
+  PASSWORD: '공백없이 8글자 이상의 비밀번호를 입력해주세요.',
 };
 
-export const validatePassword = (password: string) => {
-  return password.trim().length >= 8;
+export const validateEmail = (email: string): [boolean, string | null] => {
+  const isValid = email.trim().includes('@');
+  return [isValid, isValid ? null : ERROR_MESSAGE.EMAIL];
+};
+
+export const validatePassword = (password: string): [boolean, string | null] => {
+  const isValid = password.trim().length >= 8;
+  return [isValid, isValid ? null : ERROR_MESSAGE.PASSWORD];
 };
 
 export const isInputValid = (value: string | undefined | null) => {
