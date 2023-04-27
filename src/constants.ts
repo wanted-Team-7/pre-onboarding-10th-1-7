67f6a3b5182cf6a2f1f7c3c5ecfd8472c4df2d4a
Withlaw <car2559@naver.com>
2023-04-27 18:10:54 +0900

    constants 필요없는 변수 삭제

--- src/constants.ts
+++ src/constants.ts
@@ -11,5 +11,4 @@ export const PATH_URL = {
 export const ERROR_MESSAGE = {
   EMAIL: '공백없이 @를 포함한 올바른 이메일 주소를 입력해주세요.',
   PASSWORD: '공백없이 8글자 이상의 비밀번호를 입력해주세요.',
-  LOGIN_SUCCESSED: '로그인에 성공하셨습니다.',
 };
